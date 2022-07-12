40867ef6cf7861c4eccae8e8602384c374765f4a
Jithendra Naladala <90308408+jithendra319@users.noreply.github.com>
2022-07-12 20:17:18 +0530

    Add files via upload

---
+++ src/main/java/com/crud1/Repo/Studentrepo.java
@@ -0,0 +1,9 @@
+package com.crud1.Repo;
+
+import com.crud1.student;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Studentrepo extends CrudRepository<student, Integer>{
+
+}
---
+++ src/main/java/com/crud1/controller/Homecontroller.java
@@ -0,0 +1,43 @@
+package com.crud1.controller;
+
+import java.util.Optional;
+
+import com.crud1.student;
+import com.crud1.Repo.Studentrepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Homecontroller {
+	@Autowired
+	Studentrepo repo;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/insert")
+	public String insert(student s1) {
+		repo.save(s1);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/getstudent")
+	public ModelAndView getstudent(@RequestParam int rno) {
+		
+		ModelAndView mv=new ModelAndView("showstudent.jsp");
+		student stud=repo.findById(rno).orElse(new student());
+		mv.addObject(stud);
+		return mv;
+	}
+	
+	
+}
